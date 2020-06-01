069c92811f35893e1a4833f498af4972b010a932
syutan <61220629+syutan@users.noreply.github.com>
2020-06-01 17:15:08 +0800

    NEW

--- main.js
+++ main.js
@@ -1,201 +1,199 @@
-(function () {
+window.boot = function () {
+    var settings = window._CCSettings;
+    window._CCSettings = undefined;
 
-    'use strict';
+    if ( !settings.debug ) {
+        var uuids = settings.uuids;
 
-    function boot () {
-
-        var settings = window._CCSettings;
-        window._CCSettings = undefined;
-
-        if ( !settings.debug ) {
-            var uuids = settings.uuids;
-
-            var rawAssets = settings.rawAssets;
-            var assetTypes = settings.assetTypes;
-            var realRawAssets = settings.rawAssets = {};
-            for (var mount in rawAssets) {
-                var entries = rawAssets[mount];
-                var realEntries = realRawAssets[mount] = {};
-                for (var id in entries) {
-                    var entry = entries[id];
-                    var type = entry[1];
-                    // retrieve minified raw asset
-                    if (typeof type === 'number') {
-                        entry[1] = assetTypes[type];
-                    }
-                    // retrieve uuid
-                    realEntries[uuids[id] || id] = entry;
+        var rawAssets = settings.rawAssets;
+        var assetTypes = settings.assetTypes;
+        var realRawAssets = settings.rawAssets = {};
+        for (var mount in rawAssets) {
+            var entries = rawAssets[mount];
+            var realEntries = realRawAssets[mount] = {};
+            for (var id in entries) {
+                var entry = entries[id];
+                var type = entry[1];
+                // retrieve minified raw asset
+                if (typeof type === 'number') {
+                    entry[1] = assetTypes[type];
                 }
+                // retrieve uuid
+                realEntries[uuids[id] || id] = entry;
             }
+        }
 
-            var scenes = settings.scenes;
-            for (var i = 0; i < scenes.length; ++i) {
-                var scene = scenes[i];
-                if (typeof scene.uuid === 'number') {
-                    scene.uuid = uuids[scene.uuid];
-                }
+        var scenes = settings.scenes;
+        for (var i = 0; i < scenes.length; ++i) {
+            var scene = scenes[i];
+            if (typeof scene.uuid === 'number') {
+                scene.uuid = uuids[scene.uuid];
             }
+        }
 
-            var packedAssets = settings.packedAssets;
-            for (var packId in packedAssets) {
-                var packedIds = packedAssets[packId];
-                for (var j = 0; j < packedIds.length; ++j) {
-                    if (typeof packedIds[j] === 'number') {
-                        packedIds[j] = uuids[packedIds[j]];
-                    }
-                }
-            }
-
-            var subpackages = settings.subpackages;
-            for (var subId in subpackages) {
-                var uuidArray = subpackages[subId].uuids;
-                if (uuidArray) {
-                    for (var k = 0, l = uuidArray.length; k < l; k++) {
-                        if (typeof uuidArray[k] === 'number') {
-                            uuidArray[k] = uuids[uuidArray[k]];
-                        }
-                    }
+        var packedAssets = settings.packedAssets;
+        for (var packId in packedAssets) {
+            var packedIds = packedAssets[packId];
+            for (var j = 0; j < packedIds.length; ++j) {
+                if (typeof packedIds[j] === 'number') {
+                    packedIds[j] = uuids[packedIds[j]];
                 }
             }
         }
 
-        // init engine
-        var canvas;
+        var subpackages = settings.subpackages;
+        for (var subId in subpackages) {
+            var uuidArray = subpackages[subId].uuids;
+            if (uuidArray) {
+                for (var k = 0, l = uuidArray.length; k < l; k++) {
+                    if (typeof uuidArray[k] === 'number') {
+                        uuidArray[k] = uuids[uuidArray[k]];
+                    }
+                }
+            }
+        }
+    }
+
+    function setLoadingDisplay () {
+        // Loading splash scene
+        var splash = document.getElementById('splash');
+        var progressBar = splash.querySelector('.progress-bar span');
+        cc.loader.onProgress = function (completedCount, totalCount, item) {
+            var percent = 100 * completedCount / totalCount;
+            if (progressBar) {
+                progressBar.style.width = percent.toFixed(2) + '%';
+            }
+        };
+        splash.style.display = 'block';
+        progressBar.style.width = '0%';
+
+        cc.director.once(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function () {
+            splash.style.display = 'none';
+        });
+    }
+
+    var onStart = function () {
+        cc.loader.downloader._subpackages = settings.subpackages;
+
+        cc.view.enableRetina(true);
+        cc.view.resizeWithBrowserSize(true);
 
         if (cc.sys.isBrowser) {
-            canvas = document.getElementById('GameCanvas');
+            setLoadingDisplay();
         }
 
-        function setLoadingDisplay () {
-            // Loading splash scene
-            cc.loader.onProgress = function (completedCount, totalCount, item) {
-                var progress = 100 * completedCount / totalCount;
-                FBInstant.setLoadingProgress(progress);
-            };
+        if (cc.sys.isMobile) {
+            if (settings.orientation === 'landscape') {
+                cc.view.setOrientation(cc.macro.ORIENTATION_LANDSCAPE);
+            }
+            else if (settings.orientation === 'portrait') {
+                cc.view.setOrientation(cc.macro.ORIENTATION_PORTRAIT);
+            }
+            cc.view.enableAutoFullScreen([
+                cc.sys.BROWSER_TYPE_BAIDU,
+                cc.sys.BROWSER_TYPE_WECHAT,
+                cc.sys.BROWSER_TYPE_MOBILE_QQ,
+                cc.sys.BROWSER_TYPE_MIUI,
+            ].indexOf(cc.sys.browserType) < 0);
         }
 
-        var onStart = function () {
-            cc.view.resizeWithBrowserSize(true);
-            cc.view.enableRetina(true);
-            if (cc.sys.isBrowser) {
-                setLoadingDisplay();
-            }
+        // Limit downloading max concurrent task to 2,
+        // more tasks simultaneously may cause performance draw back on some android system / browsers.
+        // You can adjust the number based on your own test result, you have to set it before any loading process to take effect.
+        if (cc.sys.isBrowser && cc.sys.os === cc.sys.OS_ANDROID) {
+            cc.macro.DOWNLOAD_MAX_CONCURRENT = 2;
+        }
 
-            if (cc.sys.isMobile) {
-                if (settings.orientation === 'landscape') {
-                    cc.view.setOrientation(cc.macro.ORIENTATION_LANDSCAPE);
-                }
-                else if (settings.orientation === 'portrait') {
-                    cc.view.setOrientation(cc.macro.ORIENTATION_PORTRAIT);
-                }
-                // qq, wechat, baidu
-                cc.view.enableAutoFullScreen(
-                    cc.sys.browserType !== cc.sys.BROWSER_TYPE_BAIDU &&
-                    cc.sys.browserType !== cc.sys.BROWSER_TYPE_WECHAT &&
-                    cc.sys.browserType !== cc.sys.BROWSER_TYPE_MOBILE_QQ
-                );
-            }
-
-            // Limit downloading max concurrent task to 2,
-            // more tasks simultaneously may cause performance draw back on some android system / brwosers.
-            // You can adjust the number based on your own test result, you have to set it before any loading process to take effect.
-            if (cc.sys.isBrowser && cc.sys.os === cc.sys.OS_ANDROID) {
-                cc.macro.DOWNLOAD_MAX_CONCURRENT = 2;
-            }
-
-            var launchScene = settings.launchScene;
-            cc.director.preloadScene(launchScene,
-                function () {
-                    // Once all assets are loaded, tells the SDK
-                    // to end loading view and start the game
-                    FBInstant.startGameAsync()
-                    .then(function () {
-                        // Retrieving context and player information can only be done
-                        // once startGameAsync() resolves
-                        cc.director.loadScene(launchScene,
-                            function () {
-                                console.log('Success to load scene: ' + launchScene);
-                                cc.loader.onProgress = null;
+        function loadScene(launchScene) {
+            cc.director.loadScene(launchScene,
+                function (err) {
+                    if (!err) {
+                        if (cc.sys.isBrowser) {
+                            // show canvas
+                            var canvas = document.getElementById('GameCanvas');
+                            canvas.style.visibility = '';
+                            var div = document.getElementById('GameDiv');
+                            if (div) {
+                                div.style.backgroundImage = '';
                             }
-                        );
-                    })
-                    .catch(function (e) { cc.error(e); });
+                        }
+                        cc.loader.onProgress = null;
+                        console.log('Success to load scene: ' + launchScene);
+                    }
+                    else if (CC_BUILD) {
+                        setTimeout(function () {
+                            loadScene(launchScene);
+                        }, 1000);
+                    }
                 }
             );
-        };
 
-        // jsList
-        var jsList = settings.jsList;
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.js';
-        if (jsList) {
-            jsList = jsList.map(function (x) { return 'src/' + x; });
-            jsList.push(bundledScript);
-        }
-        else {
-            jsList = [bundledScript];
         }
 
-        var option = {
-            //width: width,
-            //height: height,
-            id: 'GameCanvas',
-            scenes: settings.scenes,
-            debugMode: settings.debug ? cc.debug.DebugMode.INFO : cc.debug.DebugMode.ERROR,
-            showFPS: settings.debug,
-            frameRate: 60,
-            jsList: jsList,
-            groupList: settings.groupList,
-            collisionMatrix: settings.collisionMatrix,
-        };
+        var launchScene = settings.launchScene;
 
-        // init assets
-        cc.AssetLibrary.init({
-            libraryPath: 'res/import',
-            rawAssetsBase: 'res/raw-',
-            rawAssets: settings.rawAssets,
-            packedAssets: settings.packedAssets,
-            md5AssetsMap: settings.md5AssetsMap,
-            subpackages: settings.subpackages
+        // load scene
+        loadScene(launchScene);
+
+    };
+
+    // jsList
+    var jsList = settings.jsList;
+
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.js';
+    if (jsList) {
+        jsList = jsList.map(function (x) {
+            return 'src/' + x;
         });
-        
-        cc.game.run(option, onStart);
+        jsList.push(bundledScript);
+    }
+    else {
+        jsList = [bundledScript];
     }
 
-    if (window.document) {
-        var debug = window._CCSettings.debug;
-        function loadScript (moduleName, cb) {
-            function scriptLoaded () {
-                document.body.removeChild(domScript);
-                domScript.removeEventListener('load', scriptLoaded, false);
-                cb && cb();
-            };
-            var domScript = document.createElement('script');
-            domScript.async = true;
-            domScript.src = moduleName;
-            domScript.addEventListener('load', scriptLoaded, false);
-            document.body.appendChild(domScript);
-        }
+    var option = {
+        id: 'GameCanvas',
+        scenes: settings.scenes,
+        debugMode: settings.debug ? cc.debug.DebugMode.INFO : cc.debug.DebugMode.ERROR,
+        showFPS: settings.debug,
+        frameRate: 60,
+        jsList: jsList,
+        groupList: settings.groupList,
+        collisionMatrix: settings.collisionMatrix,
+    };
 
-        loadScript(debug ? 'cocos2d-js.js' : 'cocos2d-js-min.js', function () {
-            if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
-                loadScript(debug ? 'physics.js' : 'physics-min.js', function () {
-                    FBInstant.initializeAsync().then(function () {
-                        if (typeof VConsole !== 'undefined') {
-                            window.vConsole = new VConsole();
-                        }
-                        boot();
-                    });
-                });
-            }
-            else {
-                FBInstant.initializeAsync().then(function () {
-                    if (typeof VConsole !== 'undefined') {
-                        window.vConsole = new VConsole();
-                    }
-                    boot();
-                });
-            }
-        });
+    // init assets
+    cc.AssetLibrary.init({
+        libraryPath: 'res/import',
+        rawAssetsBase: 'res/raw-',
+        rawAssets: settings.rawAssets,
+        packedAssets: settings.packedAssets,
+        md5AssetsMap: settings.md5AssetsMap,
+        subpackages: settings.subpackages
+    });
+
+    cc.game.run(option, onStart);
+};
+
+if (window.jsb) {
+    var isRuntime = (typeof loadRuntime === 'function');
+    if (isRuntime) {
+        require('src/settings.js');
+        require('src/cocos2d-runtime.js');
+        if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
+            require('src/physics.js');
+        }
+        require('jsb-adapter/engine/index.js');
+    }
+    else {
+        require('src/settings.js');
+        require('src/cocos2d-jsb.js');
+        if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
+            require('src/physics.js');
+        }
+        require('jsb-adapter/jsb-engine.js');
     }
 
-})();
+    cc.macro.CLEANUP_IMAGE_CACHE = true;
+    window.boot();
+}
